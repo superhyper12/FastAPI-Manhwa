dac621ffb4350b56405e6fe2850a1e2f0bbafc08
Egg Roll <hydrohype12@gmail.com>
2026-01-19 23:28:34 -0500

    Update ManhwaModel to allow optional fields for description, chapters, and access

--- Manhwa_DB.py
+++ Manhwa_DB.py
@@ -1,13 +1,12 @@
 from pydantic import BaseModel, Field
-
+from routes_function import *
 
 Manhwa_DB = {
     "Manhwa_A" : {"description" : "Postem Morem", "Chapter" : 10, "access": "PREMIUM", "releaseDate": "9/28/2025"}
 } 
 
 class ManhwaModel(BaseModel):
-    description: str 
-    chapters: int 
-    access: str 
-    recentDate: str
+    description: str | None
+    chapters: int | None
+    access: str | None
 
